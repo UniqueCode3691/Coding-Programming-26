115a65c09d69d919281100bd910fbddc5885c52e
Sam Mergu <samanyu3691@gmail.com>
2025-10-08 17:06:12 -0400

    Tryna fix stuff

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='font-bold text-4xl'>This is the home page(Hi Naman and Salil)!</p>
-      <img src={reactLogo} alt="react logo" className='w-40' />
-      <img src={viteLogo} alt="vite logo" className='w-20' />
+      <p className='font-bold text-3xl'>This is the home page(Hi Naman and Salil)!</p>
+      <img src={reactLogo} alt="react logo" className='w-100' />
     </>
   )
 }
